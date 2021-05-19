46e5ec5413e5f04e3e24a21fbe9362d310c4cbdb
jpsm83 <jpsm83@hotmail.com>
2021-05-19 10:23:59 +0200

    deploy

--- app.js
+++ app.js
@@ -11,11 +11,25 @@ require('./configs/cors.config')(app);
 require('./configs/session.config')(app);
 require('./configs/passport.config')(app);
 
+mongoose
+  .connect(
+  	process.env.DBURL, 
+  	{
+    	keepAlive: true,
+    	useNewUrlParser: true,
+    	useUnifiedTopology: true
+  	}
+	);
+
 const recipeRouter = require('./routes/recipes.routes');
 const authRouter = require('./routes/auth.routes');
 
 app.use('/api/recipes', recipeRouter);
 app.use('/api/auth', authRouter);
+
+app.use((req, res) => {
+  res.sendFile(__dirname + "public/index.html")
+});
 
 app.use((req, res, next) => {
   return res.status(404).json({ message: "Not found"});
