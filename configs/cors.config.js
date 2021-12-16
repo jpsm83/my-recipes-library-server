91ceb61f59860e7fe6b6d8899c59df2d7122adeb
jpsm83 <jpsm83@hotmail.com>
2021-12-16 13:52:00 +0100

    f

--- configs/cors.config.js
+++ configs/cors.config.js
@@ -2,7 +2,6 @@
 // away from the same origin policy adopted by the browsers to restrict access
 // from one domain to resources belonging to another domain
 
-
 const cors = require("cors");
 
 module.exports = (app) => {
@@ -13,9 +12,7 @@ module.exports = (app) => {
       origin: [
         process.env.PUBLIC_DOMAIN,
         // must setup coors to alow your production webpage to acess information
-        "https://vercel.com/jpsm83/my-recipes-library/9MekZfT9NcL1etSGQS6AEoU24fUT",
         "https://my-recipes-library.vercel.app",
-        "https://my-recipes-library-3qe1ysojh-jpsm83.vercel.app",
       ],
     })
   );
